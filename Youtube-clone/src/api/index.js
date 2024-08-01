b90f5beeab84a1bc9474534ac1db7f372681ec39
sarnab25 <ghoshsarnab@gmail.com>
2024-08-01 20:55:16 +0530

    Chnaged port

--- Youtube-clone/src/api/index.js
+++ Youtube-clone/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:4040/' });
+const API = axios.create({ baseURL: 'https://playin.onrender.com' });
 
 API.interceptors.request.use(req => {
     if (localStorage.getItem('Profile')) {
